f9a88004d2e5e9b8e72999d764abe19dbfe3ef54
yuanliyuanwai <wanzhengchong@qq.com>
2020-10-07 17:59:54 +0800

    junit test

--- spring-boot2/spring-boot-examples-master/spring-boot-hello/src/test/java/in/xiaocao/hello/controller/HelloTests.java
+++ spring-boot2/spring-boot-examples-master/spring-boot-hello/src/test/java/in/xiaocao/hello/controller/HelloTests.java
@@ -30,7 +30,7 @@ public class HelloTests {
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Hello World")));
+                .andExpect(content().string(equalTo("Hello World.")));
     }
 
 }
